4e3d4122cbe3d08fe3675262f2a9f319bd61802f
balintcsala <s8.boxer@gmail.com>
2017-12-01 16:34:59 +0100

    Processed data

---
+++ src/me/defenestration/ericsson/Player.java
@@ -0,0 +1,19 @@
+package me.defenestration.ericsson;
+
+public class Player {
+
+    public class Unit {
+        public int x = 0;
+        public int y = 0;
+        public int vx = 0;
+        public int vy = 0;
+        public int id = -1;
+    }
+
+    public Unit[] units = new Unit[2];
+
+    public Player(Unit[] units) {
+        this.units = units;
+    }
+
+}
